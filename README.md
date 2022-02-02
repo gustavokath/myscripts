ae94600a5bca1ed08dd378d05f2e5d935aa23e35
Gustavo Kath <gustavo.kath@sap.com>
2022-02-02 17:54:47 -0300

    Initial commit with git-fork-branch-switch

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Scripts
+
+The object of this repository is to store scripts that I've developed overtime to improve productivity and save time
---
+++ bash/git-fork-branch-switch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+input=$1;
+inputArray=(`echo $input | sed 's/:/\n/g'`);
+
+fork=${inputArray[0]};
+branch=${inputArray[1]};
+
+remote=(`echo $(git remote -v | grep -m1 origin) | sed 's/ /\n/g'`);
+remoteUrl=${remote[1]};
+forkUrl=(`echo $remoteUrl | sed "s/${USER^^}/$fork/g"`);
+
+git remote add "$fork" "$forkUrl";
+git fetch "$fork" "$branch";
+git switch "$branch";
+git pull "$fork" "$branch";
+
+echo "Branch switched to $branch of fork $fork";
